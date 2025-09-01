7c3aae63caf97747988276ed4e06415bc849b19c
Alaa Barakat <a.barakat@manteq-me.com>
2025-09-01 12:41:44 +0300

    add toastr for error handle

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,57 +1,53 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
 
 import { LoginResponse } from '../../../core/models/auth.dto';
 import { JWTPayload } from '../../../core/models/jwt-payload.model';
 import { UserRoles } from '../../../core/models/enums/user-roles.enum';
 import { environment } from '../../../../environments/environment';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
- 
+
 
   private readonly TOKEN_KEY = 'access_token';
   private readonly REFRESH_KEY = 'refresh_token';
-  private readonly SPECIALTY_KEY = 'doctor_specialty'; 
+  private readonly SPECIALTY_KEY = 'doctor_specialty';
   private readonly DOCTOR_ID_KEY = 'doctorId';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
-  // login(userDto: any): Observable<LoginResponse> {
-  //   return this.http.post<LoginResponse>(`${environment.apiUrl}/auth/login`, userDto).pipe(
-  //     tap(response => {
-  //       if (response?.data?.accessToken) {
-  //         this.setToken(response.data.accessToken, response.data.refreshToken);
-  //       }
-  //     })
-  //   );
-  // }
-login(userDto: any): Observable<LoginResponse> {
+  login(userDto: any): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${environment.apiUrl}/api/auth/login`, userDto).pipe(
       tap(response => {
-        if (response?.data?.accessToken) {
+        if (response?.succeeded && response.data?.accessToken) {
           this.setToken(response.data.accessToken, response.data.refreshToken);
 
           const role = this.getUserRole();
 
-          if (role === UserRoles.Doctor && response.data.doctor?.specializationID) {
-            localStorage.setItem(
-              this.SPECIALTY_KEY,
-              response.data.doctor.specializationID.toString(),
-            );
-          }
-
-          if (role === UserRoles.Doctor && response.data.doctor?.doctorID) {
-            localStorage.setItem(
-              this.DOCTOR_ID_KEY,
-              response.data.doctor.doctorID.toString(),
-            );
+          if (role === UserRoles.Doctor && response.data.doctor) {
+            const { specializationID, doctorID } = response.data.doctor;
+            if (specializationID) localStorage.setItem(this.SPECIALTY_KEY, specializationID.toString());
+            if (doctorID) localStorage.setItem(this.DOCTOR_ID_KEY, doctorID.toString());
           }
         }
+      }),
+      map(response => {
+        if (!response.succeeded || response.errors) {
+          const msg = response.errors?.detail?.join(', ') || response.message || 'Login failed';
+          throw new Error(msg);
+        }
+        return response;
+      }),
+      catchError(err => {
+        console.log(err);
+        this.toastr.error(err.error.errors.detail[0] || 'Login failed');
+        return throwError(() => err);
       })
     );
   }
@@ -59,31 +55,30 @@ login(userDto: any): Observable<LoginResponse> {
 
 
   getDoctorId(): number | null {
-  const doctorId = localStorage.getItem(this.DOCTOR_ID_KEY);
-  return doctorId ? +doctorId : null;
-}
+    const doctorId = localStorage.getItem(this.DOCTOR_ID_KEY);
+    return doctorId ? +doctorId : null;
+  }
 
-getSpecializationId(): number | null {
-  const specId = localStorage.getItem(this.SPECIALTY_KEY);
-  return specId ? +specId : null;
-}
-
-
-
-getSpecializationNameById(id: number): Observable<string> {
-  return this.http
-    .get<any>(`${environment.apiUrl}/api/Specializations?page=1&pageSize=100`)
-    .pipe(
-      // فلترة حسب الـ specializationID
-      map((res) => {
-        const item = res.data.items.find((s: any) => s.specializationID === id);
-        return item ? item.description : 'unknown';
-      })
-    );
-}
+  getSpecializationId(): number | null {
+    const specId = localStorage.getItem(this.SPECIALTY_KEY);
+    return specId ? +specId : null;
+  }
 
 
-private setToken(accessToken: string, refreshToken: string): void {
+
+  getSpecializationNameById(id: number): Observable<string> {
+    return this.http
+      .get<any>(`${environment.apiUrl}/api/Specializations?page=1&pageSize=100`)
+      .pipe(
+        map((res) => {
+          const item = res.data.items.find((s: any) => s.specializationID === id);
+          return item ? item.description : 'unknown';
+        })
+      );
+  }
+
+
+  private setToken(accessToken: string, refreshToken: string): void {
     localStorage.setItem(this.TOKEN_KEY, accessToken);
     localStorage.setItem(this.REFRESH_KEY, refreshToken);
   }
@@ -96,7 +91,7 @@ private setToken(accessToken: string, refreshToken: string): void {
     return localStorage.getItem(this.REFRESH_KEY);
   }
 
-   logout(): void {
+  logout(): void {
     localStorage.removeItem(this.TOKEN_KEY);
     localStorage.removeItem(this.REFRESH_KEY);
   }
@@ -126,22 +121,21 @@ private setToken(accessToken: string, refreshToken: string): void {
     return roles.includes(roleClaim as UserRoles);
   }
 
-getUserRole(): string | null {
-  const payload = this.getDecodedToken();
-  return payload ? payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] : null;
-}
+  getUserRole(): string | null {
+    const payload = this.getDecodedToken();
+    return payload ? payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] : null;
+  }
 
-getUserId(): string | null {
-  const payload = this.getDecodedToken();
-  return payload ? payload["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"] : null;
-}
+  getUserId(): string | null {
+    const payload = this.getDecodedToken();
+    return payload ? payload["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"] : null;
+  }
 
-getUserName(): string | null {
-  const payload = this.getDecodedToken();
-  return payload ? payload["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] : null;
-}
+  getUserName(): string | null {
+    const payload = this.getDecodedToken();
+    return payload ? payload["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] : null;
+  }
 
- // ✅ دوال خاصة بالتخصص
   setDoctorSpecialty(specialty: string): void {
     localStorage.setItem(this.SPECIALTY_KEY, specialty);
   }
